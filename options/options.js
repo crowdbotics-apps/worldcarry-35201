357c4981a5bf30c712a1d671bd2b52a5873b8a88
Crowdbotics <team@crowdbotics.com>
2022-05-13 16:18:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "worldcarry_35201",
+  url: "https://worldcarry_35201.botics.co",
+  api: "https://worldcarry_35201.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
