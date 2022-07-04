30c47bd80c157d4634970d109b18ecf3ee0f4dce
sallarrezaie <91070355+sallarrezaie@users.noreply.github.com>
2022-07-04 18:50:09 -0400

    Journey and Order On Route

--- backend/journeys/views.py
+++ backend/journeys/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -6,6 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Q
 from django.utils import timezone
 from home.filters import JourneyFilter
+from orders.models import Order
 from .serializers import JourneySerializer
 
 from .models import Journey
@@ -25,35 +27,68 @@ class JourneyViewSet(ModelViewSet):
 
     @action(detail=False, methods=['get'])
     def onroute(self, request):
+        order_id = request.query_params.get('order_id')
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response({"detail": "Invalid Journey ID"}, status=status.HTTP_400_BAD_REQUEST)
         today = timezone.now()
-        orders = request.user.orders.filter(
-            Q(status='Unpaid') | Q(status='Requested')
-        ).values('pickup_address_country', 'arrival_address_country', 'deliver_before_date')
 
-        journeys_list = Journey.objects.none()
-        for order in orders:
-            journeys = Journey.objects.filter(
-                Q(
-                    Q(date_of_journey__lt=order['deliver_before_date']) &
-                    Q(date_of_journey__gt=today)
-                ) &
+        journeys = Journey.objects.filter(
+            Q(
+                Q(date_of_journey__lt=order.deliver_before_date) &
+                Q(date_of_journey__gt=today)
+            ) &
+            (
+                Q(type="Round Trip") &
                 (
-                    Q(type="Round Trip") &
-                    (
-                        Q(departure_country=order['pickup_address_country']) &
-                        Q(arrival_country=order['arrival_address_country'])
-                    ) |
-                    (
-                        Q(departure_country=order['arrival_address_country']) &
-                        Q(arrival_country=order['pickup_address_country'])
-                    )
+                    Q(departure_country=order.pickup_address_country) &
+                    Q(arrival_country=order.arrival_address_country)
                 ) |
                 (
-                    Q(type="One Way") &
-                    Q(departure_country=order['pickup_address_country']) &
-                    Q(arrival_country=order['arrival_address_country'])
+                    Q(departure_country=order.arrival_address_country) &
+                    Q(arrival_country=order.pickup_address_country)
                 )
+            ) |
+            (
+                Q(type="One Way") &
+                Q(departure_country=order.pickup_address_country) &
+                Q(arrival_country=order.arrival_address_country)
             )
-            journeys_list = journeys_list | journeys
-        serializer = JourneySerializer(journeys_list, many=True)
+        )
+
+        serializer = JourneySerializer(journeys, many=True)
         return Response(serializer.data)
+
+        # today = timezone.now()
+        # orders = request.user.orders.filter(
+        #     Q(status='Unpaid') | Q(status='Requested')
+        # ).values('pickup_address_country', 'arrival_address_country', 'deliver_before_date')
+
+        # journeys_list = Journey.objects.none()
+        # for order in orders:
+        #     journeys = Journey.objects.filter(
+        #         Q(
+        #             Q(date_of_journey__lt=order['deliver_before_date']) &
+        #             Q(date_of_journey__gt=today)
+        #         ) &
+        #         (
+        #             Q(type="Round Trip") &
+        #             (
+        #                 Q(departure_country=order['pickup_address_country']) &
+        #                 Q(arrival_country=order['arrival_address_country'])
+        #             ) |
+        #             (
+        #                 Q(departure_country=order['arrival_address_country']) &
+        #                 Q(arrival_country=order['pickup_address_country'])
+        #             )
+        #         ) |
+        #         (
+        #             Q(type="One Way") &
+        #             Q(departure_country=order['pickup_address_country']) &
+        #             Q(arrival_country=order['arrival_address_country'])
+        #         )
+        #     )
+        #     journeys_list = journeys_list | journeys
+        # serializer = JourneySerializer(journeys_list, many=True)
+        # return Response(serializer.data)
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,12 +1,15 @@
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+
 from django.db.models import Q
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import OrderSerializer
 
+from journeys.models import Journey
 from .models import Order
 from users.authentication import ExpiringTokenAuthentication
 
@@ -25,37 +28,71 @@ class OrderViewSet(ModelViewSet):
 
     @action(detail=False, methods=['get'])
     def onroute(self, request):
+        journey_id = request.query_params.get('journey_id')
+        try:
+            journey = Journey.objects.get(id=journey_id)
+        except Journey.DoesNotExist:
+            return Response({"detail": "Invalid Journey ID"}, status=status.HTTP_400_BAD_REQUEST)
         today = timezone.now()
-        journeys = request.user.journeys.filter(
-            date_of_journey__gt=today
-        ).values('departure_country', 'arrival_country', 'type')
-        orders_list = Order.objects.none()
-        single_orders = Order.objects.none()
-        round_orders = Order.objects.none()
-        for journey in journeys:
-            if journey['type'] == 'Round Trip':
-                round_orders = Order.objects.filter(
-                    (Q(status='Unpaid') | Q(status='Requested')) &
-                    (
-                        Q(pickup_address_country=journey['departure_country']) &
-                        Q(arrival_address_country=journey['arrival_country'])
-                    ) |
-                    Q(
-                        Q(pickup_address_country=journey['arrival_country']) &
-                        Q(arrival_address_country=journey['departure_country'])
-                    )
+
+        if journey.date_of_journey < today.date():
+            return Response({"detail": "This Journey has already passed its departure date"},
+                             status=status.HTTP_400_BAD_REQUEST)
+        
+        if journey.type == 'Round Trip':
+            orders = Order.objects.filter(
+                (Q(status='Unpaid') | Q(status='Requested')) &
+                (
+                    Q(pickup_address_country=journey.departure_country) &
+                    Q(arrival_address_country=journey.arrival_country)
+                ) |
+                Q(
+                    Q(pickup_address_country=journey.arrival_country) &
+                    Q(arrival_address_country=journey.departure_country)
                 )
-            else:
-                single_orders = Order.objects.filter(
-                    Q(status='Unpaid') &
-                    (
-                        Q(pickup_address_country=journey['departure_country']) &
-                        Q(arrival_address_country=journey['arrival_country'])
-                    )
+            )
+        else:
+            orders = Order.objects.filter(
+                Q(status='Unpaid') &
+                (
+                    Q(pickup_address_country=journey.departure_country) &
+                    Q(arrival_address_country=journey.arrival_country)
                 )
-            orders_list = orders_list | round_orders | single_orders
-            single_orders = Order.objects.none()
-            round_orders = Order.objects.none()
-        serializer = OrderSerializer(orders_list, many=True)
+            )
+
+        serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
+
+        # journeys = request.user.journeys.filter(
+        #     date_of_journey__gt=today
+        # ).values('departure_country', 'arrival_country', 'type')
+        # orders_list = Order.objects.none()
+        # single_orders = Order.objects.none()
+        # round_orders = Order.objects.none()
+        # for journey in journeys:
+        #     if journey['type'] == 'Round Trip':
+        #         round_orders = Order.objects.filter(
+        #             (Q(status='Unpaid') | Q(status='Requested')) &
+        #             (
+        #                 Q(pickup_address_country=journey['departure_country']) &
+        #                 Q(arrival_address_country=journey['arrival_country'])
+        #             ) |
+        #             Q(
+        #                 Q(pickup_address_country=journey['arrival_country']) &
+        #                 Q(arrival_address_country=journey['departure_country'])
+        #             )
+        #         )
+        #     else:
+        #         single_orders = Order.objects.filter(
+        #             Q(status='Unpaid') &
+        #             (
+        #                 Q(pickup_address_country=journey['departure_country']) &
+        #                 Q(arrival_address_country=journey['arrival_country'])
+        #             )
+        #         )
+        #     orders_list = orders_list | round_orders | single_orders
+        #     single_orders = Order.objects.none()
+        #     round_orders = Order.objects.none()
+        # serializer = OrderSerializer(orders_list, many=True)
+        # return Response(serializer.data)
         
