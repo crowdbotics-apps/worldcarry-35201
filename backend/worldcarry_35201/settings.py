e1cc6cf4d4ca82ca10f21b4db410260b3b450d8a
husnainyousaf-cheetay <husnain.yousaf@cheetay.pk>
2022-07-20 23:32:27 +0500

    Updated pip file

--- backend/worldcarry_35201/settings.py
+++ backend/worldcarry_35201/settings.py
@@ -48,7 +48,8 @@ except (DefaultCredentialsError, PermissionDenied):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("HOST", default=["*"])
+# ALLOWED_HOSTS = env.list("HOST", default=["*"])
+ALLOWED_HOSTS = ['*']
 SITE_ID = 1
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
@@ -132,18 +133,29 @@ WSGI_APPLICATION = 'worldcarry_35201.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# if env.str("DATABASE_URL", default=None):
+#     DATABASES = {
+#         'default': env.db_url(engine='django.contrib.gis.db.backends.postgis')
+#     }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
+'default': {
+    'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    'NAME': 'postgres',
+    'USER': 'postgres',
+    'PASSWORD': 'postgres',
+    'HOST': 'localhost',
+    'PORT': '5432',
 }
-
-if env.str("DATABASE_URL", default=None):
-    DATABASES = {
-        'default': env.db_url(engine='django.contrib.gis.db.backends.postgis')
-    }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
