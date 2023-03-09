2ea29dff616d5ac4c4038884c9e48d46532925be
milyasyousuf <m_ilyas@outlook.com>
2023-03-09 14:01:25 +0500

    Journey models added in admin

--- backend/journeys/admin.py
+++ backend/journeys/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from journeys.models import Journey, JourneyOrder, DeclineJourneyOrder
+
 # Register your models here.
+admin.site.register(Journey)
+admin.site.register(JourneyOrder)
+admin.site.register(DeclineJourneyOrder)
